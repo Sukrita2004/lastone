5a3450800b4c080e922582e038922ac74339a052
Sukrita2004 <Fame2047@gmail.com>
2025-09-02 17:02:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -101,4 +101,7 @@ def predict():
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
+
